6e84e64f779926464a8315839d781c6ac6d3e8a9
Mitch Richling <11151403+richmit@users.noreply.github.com>
2026-07-31 14:29:14 -0500

    Updated readme

--- mjr-meta-eval.el
+++ mjr-meta-eval.el
@@ -19,17 +19,54 @@
 ;; TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 ;; Author:      Mitch Richling
-;; Version:     0.8
+;; Version:     0.9
 ;; Keywords:    mjr-meta-eval
 ;; URL:         https://github.com/richmit/mjr-meta-eval
 
 ;; This file is not part of Emacs
 
-;;; Install:
-;; See the README: https://github.com/richmit/mjr-meta-eval/
-
 ;;; Commentary:
+;;
 ;; See the README: https://github.com/richmit/mjr-meta-eval/
+;;
+;; The Emacs function `mjr-meta-eval' provides an easy way to evaluate an expression in a buffer and place the results on the kill ring.  My primary use case
+;; is the evaluation of mathematical expressions embedded in source code.
+;;
+;; The expression may be evaluated in several different ways:
+;;
+;;   - :int ..... Uses `mjr-meta-eval-multibase-convert'
+;;   - :calc .... like calling `quick-calc' (C-c * q)
+;;   - :elisp ... like calling `eval-expression' (M-:)
+;;   - :lisp .... Uses common lisp via SLIME
+;;   - :maxima .. Uses maxima 
+;;   - :octave .. Uses octave 
+;;
+;; The expression can be identified by highlighting it in the buffer.  Alternately `mjr-meta-eval' will look around the point for something to evaluate.  In
+;; lisp and lisp adjacent modes, look for a sexp at the point.  In non-lisp modes, look a string not containing spaces.  Whenever `mjr-meta-eval' makes a
+;; guess about what to evaluate, it always gives the user the option to edit the expression before evaluation.
+;;
+;; If the string to be evaluated looks like a single integer, then :int evaluation is always used.  Integers in many common programming languages can be
+;; recognized:
+;;
+;;   - Unadorned, decimal integers in most programming languages.
+;;   - C/C++
+;;     - Integer suffixes (unsigned * long) are supported
+;;     - Integer prefixes for HEX, BIN, & OCT are also supported
+;;   - BOZ (BIN, OCT, & HEX) integer literals in F77 & F90.
+;;   - LISP/ELISP read macros for (BIN, OCT, DEC, & HEX)
+;;   - Native Emacs CALC syntax.
+;;   - Numeric string escape sequences in C++ and Java."
+;;
+;; I normally bind `mjr-meta-eval' to "ESC ESC :" -- that is hit escape twice and then colon.  I think of `mjr-meta-eval' as an extended version of
+;; `eval-expression' that is normally bound to "ESC :".
+;;
+;;      (keymap-global-set "ESC ESC :"   'mjr-meta-eval)
+;;            
+;; The easiest way to install mjr-meta-eval is to pull it directly from github:
+;;
+;;      (package-vc-install (list 'mjr-meta-eval
+;;                           :url "https://github.com/richmit/mjr-meta-eval"
+;;                           :rev 'newest))
 
 ;;; Code:
 
