87d80bbe78efe5e1744ccce43d4e49cf8a0ed35e
Mitch Richling <11151403+richmit@users.noreply.github.com>
2026-08-17 17:17:27 -0500

    Updated readme

--- mjr-eval.el
+++ mjr-eval.el
@@ -43,43 +43,37 @@
 ;;  - Paste the result
 ;;  
 ;; This package aims to replace the ghastly practice described above with an efficient and uniform user interface:
-;;   - One key binding to access all the computational tools.
-;;   - Helps to identify buffer contents for evaluation.
-;;   - Provides a fast way for the user to select which computational tool to use (just a single key press)
-;;   - Evaluates the expression and both prints the results and places them on the kill ring.
+;;  - One key binding to access all the computational tools.
+;;  - Helps to identify buffer contents for evaluation.
+;;  - Provides a fast way for the user to select which computational tool to use (just a single key press)
+;;  - Evaluates the expression and both prints the results and places them on the kill ring.
 ;;
 ;; This package uses a couple terms:
-;;   - ENGINE to mean a computational tools (i.e. octave is an engine as is `calc').  
-;;   - HANDLER is a function that takes a string to evaluate and a symbol identifying an ENGINE.
+;;  - ENGINE to mean a computational tools (i.e. octave is an engine as is `calc').  
+;;  - HANDLER is a function that takes a string to evaluate and a symbol identifying an ENGINE.
 ;;
 ;; This package provides four primary HANDLERs and a single dispatch function that can be used as an interface to them all.  The four HANDLERs are:
 ;;
-;;   `mjr-eval-org-ticker'
-;;      Use `org-mode' babel for all computations.
-;;        - Pro: Keeps a history of all computations preformed during the session
-;;        - Con: Requires a working `org-mode' configuration and babel configuration for each language used
-;;        - Con: Babel support for some engines may be limited.
+;;  - `mjr-eval-org-ticker' -- Use `org-mode' babel for all computations.
+;;     - Pro: Keeps a history of all computations preformed during the session
+;;     - Con: Requires a working `org-mode' configuration and babel configuration for each language used
+;;     - Con: Babel support for some engines may be limited.
 ;;    
-;;   `mjr-eval-external-session'
-;;      Use an external tool via the author's favorite Emacs mode for that tool
-;;        - Pro: Uses interactive modes that are very popular
-;;        - Con: Not everybody is going to like the choice of which mode was used to provide support
-;;        - Pro: For users already running sessions with these tools, this packge provides an alternate interface for interacting with sessions.
-;;        - Con: Somewhat flaky support for :octave & :r
+;;  - `mjr-eval-external-session' -- Use an external tool via the author's favorite Emacs mode for that tool
+;;    - Pro: Uses interactive modes that are very popular
+;;    - Con: Not everybody is going to like the choice of which mode was used to provide support
+;;    - Pro: For users already running sessions with these tools, this packge provides an alternate interface for interacting with sessions.
+;;    - Con: Somewhat flaky support for :octave & :r
 ;;    
-;;   `mjr-eval-internal'
-;;      Use internal Emacs facilities
+;;  - `mjr-eval-internal'-- Use internal Emacs facilities
+;;    - Pro: Requires zero configuration
+;;    - Pro: Lowest latency results
+;;    - Con: Fewer & less powerful engines
 ;;    
-;;        - Pro: Requires zero configuration
-;;        - Pro: Lowest latency results
-;;        - Con: Fewer & less powerful engines
-;;    
-;;   `mjr-eval-external-one'
-;;      Use an external tool by directly calling the binary
-;;    
-;;        - Pro: Super simple and very robust
-;;        - Con: High latency
-;;        - Pro/Con: No session support (which also means no session to crash or freeze)
+;;  - `mjr-eval-external-one'-- Use an external tool by directly calling the binary
+;;    - Pro: Super simple and very robust
+;;    - Con: High latency
+;;    - Pro/Con: No session support (which also means no session to crash or freeze)
 ;;
 ;; While all of the HANDLERs can be used interactively, it is probably more convenient to access them via the dispatch function `mjr-eval-meta'.  This
 ;; function has a more sophisticated user interface and allows the user to customize what tools are available.
